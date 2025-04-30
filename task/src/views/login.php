372f90c972d4870eceab47c1c2cfbb322f53003c
Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2025-04-30 10:47:29 +0200

    Update login.php

--- task/src/views/login.php
+++ task/src/views/login.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <h1>Login</h1>
+    <?php if (isset($error)): ?>
+        <p class="error"><?php echo htmlspecialchars($error); ?></p>
+    <?php endif; ?>
+    <form method="POST">
+        <label>Username: <input type="text" name="username" required></label>
+        <label>Password: <input type="password" name="password" required></label>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
