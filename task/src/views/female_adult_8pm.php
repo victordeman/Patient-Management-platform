770d4201e6dc91a2679c3180985f4f716c881bfa
Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2025-04-30 10:48:03 +0200

    Update female_adult_8pm.php

--- task/src/views/female_adult_8pm.php
+++ task/src/views/female_adult_8pm.php
@@ -1,1 +1,20 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Female Adult Patients (8 PM)</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <h1>Female Adult Patients (8 PM Medications)</h1>
+    <ul>
+        <?php if (empty($results)): ?>
+            <li>No data available.</li>
+        <?php else: ?>
+            <?php foreach ($results as $row): ?>
+                <li><?php echo htmlspecialchars($row['name']); ?>: <?php echo htmlspecialchars($row['medicine_name']); ?></li>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </ul>
+    <p><a href="?route=home">Back to Home</a> | <a href="?route=logout">Logout</a></p>
+</body>
+</html>
